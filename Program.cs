1cbd418bb2d6ba0712bf397d2df246da25e76cca
sarah sida <nestsida@gmail.com>
2021-10-08 10:35:40 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Class.Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newCar = new Car();
+
+            newCar.Make = "Audi";
+            newCar.Model = "A4";
+            newCar.Year = 2020;
+
+            Console.WriteLine($"Make: {newCar.Make}");
+            Console.WriteLine($"Model: {newCar.Model}");
+            Console.WriteLine($"Year: {newCar.Year}");
+        }
+    }
+}
